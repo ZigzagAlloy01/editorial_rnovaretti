65f0ed2e2450400e81193d42747f4d928575b996
ZigzagAlloy01 <ivanblued@gmail.com>
2026-01-22 09:28:10 -0700

    Cambios para vercel

---
+++ api/index.js
@@ -0,0 +1,40 @@
+import express from "express";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+
+// Fix __dirname
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// Views
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "../views"));
+
+// Middlewares
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/servicios", (req, res) => {
+  res.render("services");
+});
+
+app.get("/quienes-somos", (req, res) => {
+  res.render("about");
+});
+
+app.get("/portafolio", (req, res) => {
+  res.render("portfolio");
+});
+
+app.get("/contacto", (req, res) => {
+  res.render("contact");
+});
+
+export default app;
