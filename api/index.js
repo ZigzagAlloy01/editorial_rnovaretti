fde197aa24e0acc85c0dc4522aa64b0698e86902
ZigzagAlloy01 <ivanblued@gmail.com>
2026-01-25 16:20:55 -0700

    app.use(session

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import path from "path";
 import { fileURLToPath } from "url";
+import session from "express-session";
 import portfolio from "../models/portfolio.js"
 import connectDB from "../lib/db.js";
 import adminRoutes from "../routes/admin.js";
@@ -18,7 +19,15 @@ app.set("views", path.join(__dirname, "../views"));
 // Middlewares
 app.use(express.static(path.join(__dirname, "../public")));
 app.use(express.urlencoded({ extended: true }));
-
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false,
+  cookie: { 
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true 
+  }
+}));
 // Routes
 app.get("/", (req, res) => {
   res.render("index");
