7eabe4a04447a22046cd4875d30bed8b21710ec5
ZigzagAlloy01 <ivanblued@gmail.com>
2026-06-05 11:03:02 -0600

    Primeros links de ebooks

--- routes/public.js
+++ routes/public.js
@@ -1,5 +1,6 @@
 import express from "express";
 import portfolio from "../models/portfolio.js";
+import mercadopago from "../mercadopago.json" with { type: "json" };
 
 const router = express.Router();
 
@@ -18,6 +19,20 @@ router.get("/quienes-somos", (req, res) => {
 router.get("/portafolio", async (req, res) => {
   const trabajos = await portfolio.find().lean();
   res.render("portfolio", { trabajos });
+});
+
+router.get("/comprar-ebook/:title", (req, res) => {
+
+  const title = decodeURIComponent(req.params.title);
+
+  const libro = mercadopago[title];
+
+  if (!libro) {
+    return res.status(404).send("No se encontró enlace de pago para este libro.");
+  }
+
+  res.redirect(libro.enlace_pago);
+
 });
 
 router.get("/anuncios", (req, res) => {
